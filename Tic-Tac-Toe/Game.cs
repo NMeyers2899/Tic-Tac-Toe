2e931183e902f8c2da6f21bcaec9b1f90c773110
nicholasMeyers <s218019@student.aie.edu>
2021-10-04 14:26:02 -0500

    Created a Main Game Loop and a Board Class

---
+++ Tic-Tac-Toe/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class Game
+    {
+        private bool _gameOver = false;
+
+        public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                Update();
+            }
+
+            End();
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+    }
+}
---
+++ Tic-Tac-Toe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void AddRows(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    int rowSum = 0;
+
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        rowSum += array[i, j];
+                    }
+
+                    Console.WriteLine(rowSum);
+                }
+            }
+
+            int[,] grid = new int[3, 3] { { 1, 7, 3 }, 
+                                          { 42, 412, 6 }, 
+                                          { 7, 123, 12 } };
+
+            AddRows(grid);
+        }
+    }
+}
